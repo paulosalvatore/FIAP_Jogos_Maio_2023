02f6a64921c91c1cf6403a00316f2b2766e42a18
Paulo Salvatore <salvatoregames@gmail.com>
2023-05-02 21:31:35 -0300

    feat(player): implement movement

--- Assets/Scripts/PlayerMoveController.cs
+++ Assets/Scripts/PlayerMoveController.cs
@@ -1,18 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMoveController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Range(0.1f, 2)]
+    public float moveSpeed = 1;
+
+    [SerializeField]
+    private Rigidbody2D rb;
+
+    private Vector2 _moveInput;
+    private Vector2 _moveDirection;
+
+    private void HandleMovement()
     {
-        
+        _moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        _moveDirection = _moveInput.normalized;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        HandleMovement();
+        rb.velocity = new Vector2(_moveDirection.x * moveSpeed, _moveDirection.y * moveSpeed);
     }
 }
